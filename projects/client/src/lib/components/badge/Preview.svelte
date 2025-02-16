d9343abd0d699733bdd31e534e71369b1cba7901
seferturan <seferturan85@gmail.com>
2025-02-16 20:18:47 +0100

    wip: preview badge

---
+++ projects/client/src/lib/components/badge/Preview.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { tooltip } from "$lib/utils/actions/tooltip";
+
+  const { description }: { description: string } = $props();
+</script>
+
+<div class="trakt-preview-badge" use:tooltip={description}>
+  <h6>Preview</h6>
+</div>
+
+<style>
+  .trakt-preview-badge {
+    border-radius: var(--border-radius-l);
+
+    height: var(--ni-32);
+    padding: var(--ni-12);
+    box-sizing: border-box;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #56bbc613;
+
+    animation: rotateShadows 8s linear infinite;
+    box-shadow:
+      inset 0 0 25px #fff,
+      inset 10px 0 20px violet,
+      inset -10px 0 20px #0ff,
+      inset 10px 0 20px violet,
+      inset -10px 0 20px #0ff,
+      0 0 25px #fff,
+      -5px 0 30px violet,
+      5px 0 30px #0ff;
+  }
+
+  @keyframes rotateShadows {
+    0% {
+      filter: hue-rotate(0deg);
+    }
+    100% {
+      filter: hue-rotate(360deg);
+    }
+  }
+</style>
