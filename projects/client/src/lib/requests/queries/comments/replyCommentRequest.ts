1294beaf2674a256a72bfe9d5a6b53d6798da56c
seferturan <seferturan85@gmail.com>
2025-03-21 15:23:06 +0100

    feat(requests): add request to reply to a comment

---
+++ projects/client/src/lib/requests/queries/comments/replyCommentRequest.ts
@@ -0,0 +1,18 @@
+import { api, type ApiParams } from '$lib/requests/api.ts';
+import type { CommentReplyParams } from '@trakt/api';
+
+type ReplyCommentParams = { id: number; body: CommentReplyParams } & ApiParams;
+
+export function replyCommentRequest(
+  { fetch, id, body }: ReplyCommentParams,
+): Promise<boolean> {
+  return api({ fetch })
+    .comments
+    .reply({
+      params: {
+        id: `${id}`,
+      },
+      body,
+    })
+    .then(({ status }) => status === 204);
+}
