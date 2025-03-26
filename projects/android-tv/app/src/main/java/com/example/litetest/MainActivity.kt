385670f1c3e1725f38fff91a2d7ddf410dc8b6e7
seferturan <seferturan85@gmail.com>
2025-03-26 20:03:14 +0100

    wip: default webview project

---
+++ projects/android-tv/app/src/main/java/com/example/litetest/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.traktlite
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+
+        webView.settings.apply {
+            javaScriptEnabled = true  // Enable JavaScript
+            domStorageEnabled = true  // Enable local storage
+            // cacheMode = WebSettings.LOAD_NO_CACHE // TODO decide cache mode
+            useWideViewPort = true //enable support for viewport meta tags
+            loadWithOverviewMode = true // load webpage scaled to fit the webview
+        }
+
+        //https://developer.android.com/develop/ui/views/layout/webapps/webview
+        //TODO add javascript interfaces?
+        webView.setInitialScale(0)
+
+        webView.webViewClient = WebViewClient()
+
+        // TODO: change url based on env (dev, preview, prod
+        // 10.0.2.2 is the alias to host machine's loopback interface
+        webView.loadUrl("http://10.0.2.2:5173/")  // Replace with your actual URL
+
+        setContentView(webView)
+
+        val callback = object : OnBackPressedCallback(
+            true // default to enabled
+        ) {
+            override fun handleOnBackPressed() {
+                if (webView.copyBackForwardList().getCurrentIndex() > 0) {
+                    webView.goBack()
+                } else {
+                    this.isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                    this.isEnabled = true
+                }
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(
+            this, // LifecycleOwner
+            callback
+        )
+    }
+}
