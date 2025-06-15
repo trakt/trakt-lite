564d93ee53306c22cc82dd10d57ade1eed6c3b2c
seferturan <seferturan85@gmail.com>
2025-06-15 23:09:05 +0200

    wip: update list request

---
+++ projects/client/src/lib/requests/queries/users/updateListRequest.ts
@@ -0,0 +1,27 @@
+import { api, type ApiParams } from '$lib/requests/api.ts';
+
+type UnfollowUserRequest = {
+  userId: string;
+  listId: string;
+  name: string;
+} & ApiParams;
+
+// TODO update API
+export function updateListRequest(
+  { userId, listId, name, fetch }: UnfollowUserRequest,
+): Promise<boolean> {
+  return api({ fetch })
+    .users
+    .lists
+    .list
+    .update({
+      params: {
+        id: userId,
+        list_id: listId,
+      },
+      body: {
+        name,
+      },
+    })
+    .then(({ status }) => status === 200);
+}
