01dc6a9ff7a5827bea8f3f5d5382bc44b62fc575
seferturan <seferturan85@gmail.com>
2025-06-23 18:18:52 +0200

    feat(auth): switch to oidc-client-ts

--- projects/client/src/lib/sections/navbar/components/JoinTraktButton.svelte
+++ projects/client/src/lib/sections/navbar/components/JoinTraktButton.svelte
@@ -1,34 +1,22 @@
 <script lang="ts">
   import Button from "$lib/components/buttons/Button.svelte";
-  import { useAuth } from "$lib/features/auth/stores/useAuth";
   import * as m from "$lib/features/i18n/messages";
   import { DpadNavigationType } from "$lib/features/navigation/models/DpadNavigationType";
-  import RenderFor from "$lib/guards/RenderFor.svelte";
-  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import { useLoginTarget } from "./useLoginTarget";
 
-  const { url } = useAuth();
+  const { target } = useLoginTarget();
 </script>
 
-{#snippet joinButton(url: string)}
+<join-trakt-button>
   <Button
-    href={url}
     label={m.join_trakt_button_label()}
     style="flat"
     size="small"
     color="purple"
     variant="primary"
     navigationType={DpadNavigationType.Item}
+    {...$target}
   >
     {m.join_trakt_button()}
   </Button>
-{/snippet}
-
-<join-trakt-button>
-  <RenderFor audience="public" navigation="default">
-    {@render joinButton($url)}
-  </RenderFor>
-
-  <RenderFor audience="public" navigation="dpad">
-    {@render joinButton(UrlBuilder.login.activate())}
-  </RenderFor>
 </join-trakt-button>
